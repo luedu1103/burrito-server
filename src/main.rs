5f98177cf1724b621f70ae46a5f0e66d82271c15
luedu1103 <luis.calle3@unmsm.edu.pe>
2024-06-26 16:18:54 -0500

    work pls

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use rocket::http::Status;
 use rocket::serde::{json::Json, Deserialize, Serialize};
 use rocket::State;
 use std::sync::Mutex;
+use rocket::{Request, Response};
+use rocket::http::Header;
+use rocket::fairing::{Fairing, Info, Kind};
 
 #[macro_use] extern crate rocket;
 
@@ -55,10 +58,31 @@ fn testing() -> String{
     string
 }
 
+// CORS thing
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
     let rocket = rocket::build()
         .mount("/", routes![index, give_position, handle_options_request, get_position, testing])
+        .attach(CORS)
         .manage(PositionState::default());
 
     Ok(rocket.into())
